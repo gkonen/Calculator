67ebf225c8b7ebe57163d36f50ee002d0b2a77fb
Tsukidaitou <tsukidaitou357@gmail.com>
2022-09-28 14:27:22 +0200

    First structure and Hilt

---
+++ app/src/main/java/be/gkonen/calculator/CalculatorApp.kt
@@ -0,0 +1,8 @@
+package be.gkonen.calculator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CalculatorApp : Application() {
+}
--- app/src/main/java/be/gkonen/calculator/MainActivity.kt
+++ app/src/main/java/be/gkonen/calculator/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import be.gkonen.calculator.ui.theme.CalculatorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
