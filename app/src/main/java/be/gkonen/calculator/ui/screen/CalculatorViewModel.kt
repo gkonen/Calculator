a57338308175a946bd7e834a619740d999862db5
Tsukidaitou <tsukidaitou357@gmail.com>
2022-11-11 14:40:52 +0100

    Begin Logic for the Button

--- app/src/main/java/be/gkonen/calculator/ui/screen/CalculatorViewModel.kt
+++ app/src/main/java/be/gkonen/calculator/ui/screen/CalculatorViewModel.kt
@@ -1,6 +1,7 @@
 package be.gkonen.calculator.ui.screen
 
 import androidx.lifecycle.ViewModel
+import be.gkonen.calculator.domain.KeyboardHelper
 import be.gkonen.calculator.model.UIEvent
 import be.gkonen.calculator.model.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,13 +15,57 @@ class CalculatorViewModel @Inject constructor(): ViewModel() {
     private var _uiState = MutableStateFlow<UIState>(UIState.Idle)
     val uiState : StateFlow<UIState> = _uiState
 
+    private var _information = MutableStateFlow<String>("")
+    val information : StateFlow<String> = _information
+
     fun onEvent(event: UIEvent) {
         when(event) {
             is UIEvent.ButtonPressed -> {
+                manageButton(event.button)
                 _uiState.value = UIState.Notification(
-                    "Vous avez appuyé sur ${event.button.name} "
+                    "Vous avez appuyé sur ${event.button.name} : ${_information.value} "
                 )
             }
+        }
+
+    }
+
+    private fun manageButton(config : KeyboardHelper.ButtonConfig) {
+        when(config) {
+            KeyboardHelper.ButtonConfig.AC -> _information.value = ""
+            KeyboardHelper.ButtonConfig.POINT -> {
+            }
+            KeyboardHelper.ButtonConfig.PLUS_MINUS -> {
+
+            }
+            KeyboardHelper.ButtonConfig.PERCENT -> {
+
+            }
+            KeyboardHelper.ButtonConfig.ADD -> {
+
+            }
+            KeyboardHelper.ButtonConfig.MINUS -> {
+
+            }
+            KeyboardHelper.ButtonConfig.DIVIDE -> {
+
+            }
+            KeyboardHelper.ButtonConfig.MULTIPLY -> {
+
+            }
+            KeyboardHelper.ButtonConfig.EQUAL -> {
+
+            }
+            KeyboardHelper.ButtonConfig.B0 -> _information.value += "0"
+            KeyboardHelper.ButtonConfig.B1 -> _information.value += "1"
+            KeyboardHelper.ButtonConfig.B2 -> _information.value += "2"
+            KeyboardHelper.ButtonConfig.B3 -> _information.value += "3"
+            KeyboardHelper.ButtonConfig.B4 -> _information.value += "4"
+            KeyboardHelper.ButtonConfig.B5 -> _information.value += "5"
+            KeyboardHelper.ButtonConfig.B6 -> _information.value += "6"
+            KeyboardHelper.ButtonConfig.B7 -> _information.value += "7"
+            KeyboardHelper.ButtonConfig.B8 -> _information.value += "8"
+            KeyboardHelper.ButtonConfig.B9 -> _information.value += "9"
         }
 
     }
